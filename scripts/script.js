9e727668e42f1e304c298fc7fa4b88191e7ac2bc
EkaterinaZ <KateLegion@yandex.ru>
2021-06-25 12:49:48 +0300

    1испр

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,13 @@
 const popupOpenButton = document.querySelector('.profile__edit-button')
 const popupElement = document.querySelector('.popup')
 const popupCloseButton = popupElement.querySelector('.popup__close')
-let nameInput = popupElement.querySelector('.popup__name')
-let jobInput = popupElement.querySelector('.popup__job')
+let nameInput = popupElement.querySelector('.popup__form_name')
+let jobInput = popupElement.querySelector('.popup__form_job')
 let profileName = document.querySelector ('.profile__name')
 let profileDescription = document.querySelector ('.profile__description')
 const formElement = popupElement.querySelector('.popup__content')
 
+// функция открытия попап
 const openPopup = function(){
     popupElement.classList.add('popup_is-opened');
     nameInput.value = profileName.textContent
@@ -14,11 +15,12 @@ const openPopup = function(){
 }
 
 
-
+// функция закрытия попап
 const closePopup = function(){
     popupElement.classList.remove('popup_is-opened')
 }
 
+// ф-я закрытия попап кликом на фон
 const closePopupByClickOnOverlay = function(event) {
     console.log(event.target, event.currentTarget)
     if (event.target !== event.currentTarget) {
@@ -28,6 +30,7 @@ const closePopupByClickOnOverlay = function(event) {
     closePopup()
   }
 
+// ф-я редактирования профиля, ч/з формы в
 const formSubmitHandler = function(evt){
     evt.preventDefault()
     profileName.textContent = nameInput.value
@@ -42,4 +45,3 @@ formElement.addEventListener('submit', formSubmitHandler)
 
 
 
-console.log(formElement)
